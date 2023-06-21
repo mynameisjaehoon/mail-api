f0e78a0d68b9a32b375a4967a54e22d2acc4276d
Jaehoon-So <jhspacelover@naver.com>
2023-06-22 00:40:20 +0900

    add: add spring boot project

---
+++ src/test/java/api/mail/MailApplicationTests.java
@@ -0,0 +1,13 @@
+package api.mail;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MailApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
